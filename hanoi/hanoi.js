6e8e18b630e06660c3b43024820c691f6f0cbeb5
Neil Fraser <fraser@google.com>
2023-05-09 12:23:15 +0200

    Improve comments in Hanoi
    Also remove unneeded 'disks' array.

--- hanoi/hanoi.js
+++ hanoi/hanoi.js
@@ -5,7 +5,7 @@
  */
 
 /**
- * @fileoverview Hanoi.
+ * @fileoverview Tower of Hanoi.
  * @author root@neil.fraser.name (Neil Fraser)
  */
 'use strict';
@@ -20,11 +20,10 @@ var RAISE_HEIGHT = DISK_THICKNESS;
 var SPEED = 1.0;  // Smaller is faster.
 
 var totalDisks = 5;
-var disks = [];
-var pegX = [];
-var pegs = [[], [], []];
-var selectedDisk = undefined;
-var lastCompletePeg = 0;
+var pegX = [];  // Horizontal locations of the three pegs.
+var pegs = [[], [], []];  // Each peg holds an array of disks.
+var selectedDisk = undefined;  // Currently raised disk.
+var lastCompletePeg = 0;  // Which peg last held the entire stack.
 
 // Start-up initialization code.  Run once.
 function init() {
@@ -32,7 +31,8 @@ function init() {
 
   // Configure difficulty mode.
   var m = document.cookie.match(/disks=(\d+)/);
-  totalDisks = Number(m ? m[1] : totalDisks);
+  var cookieDisks = m ? Number(m[1]) : 0;
+  totalDisks = cookieDisks > 0 ? cookieDisks : totalDisks;
   var options =  document.getElementById('disks').options;
   for (var i = 0; i < options.length; i++) {
     options[i].selected = (options[i].value == totalDisks);
@@ -62,6 +62,7 @@ window.addEventListener('load', init);
 
 var lastTime;
 
+// Clear and redraw everything.  One animation frame.
 function frame(timestamp) {
   var delta;
   if (lastTime === undefined) {
@@ -75,7 +76,7 @@ function frame(timestamp) {
   canvas.width = canvas.width;  // Clear the canvas.
 
   ctx.translate(0, CANVAS_HEIGHT * 0.8);
-  // Draw the spots.
+  // Draw the landing spots.
   for (var i = 0; i < pegs.length; i++) {
     ctx.save();
     ctx.translate(pegX[i], 0);
@@ -98,7 +99,7 @@ function frame(timestamp) {
       }
 
       if (disk.sourceX === disk.targetX) {
-        // Straight up or down.
+        // Moving straight up or down.
         if (disk.targetY > disk.y) {
           disk.y = Math.min(disk.y + delta, disk.targetY);
         } else if (disk.targetY < disk.y) {
@@ -118,7 +119,7 @@ function frame(timestamp) {
         }
 
         // Moving parabolically to a new peg.
-        var peakX = disk.sourceX + distanceX / 2;
+        var peakX = disk.sourceX + (distanceX / 2);
         var averageY = (disk.targetY + disk.sourceY) / 2;
         var peakY = averageY + Math.abs(distanceX / peakDenominator);
         var y = computeParabolaY(disk.x,
@@ -201,31 +202,34 @@ function input(n) {
   }
 }
 
+// Are all the completely disks stacked in on a new peg?
+// If so, play the win sound.
 function checkWin() {
   for (var i = 0; i < pegs.length; i++) {
     if (lastCompletePeg !== i && pegs[i].length === totalDisks) {
       lastCompletePeg = i;
-      // Slight delay to let the animation for this move to play.
+      // Slight delay to let the animation for this move to play out.
       setTimeout(function() {
         document.getElementById('win').play();
       }, 500);
 
       // Reset disk move status.
-      for (var i = 0; i < totalDisks; i++) {
-        disks[i].hasMoved = false;
-        disks[i].isFirstMove = true;
+      for (var j = 0; j < pegs[i].length; j++) {
+        pegs[i][j].hasMoved = false;
+        pegs[i][j].isFirstMove = true;
       }
     }
   }
 
 }
 
+// Class for a disk.
 function Disk(n, total) {
-  this.n = n;
+  this.n = n;  // 0 is the smallest disk.
   var maxRadius = CANVAS_WIDTH / 6;
   var minRadius = maxRadius / 10;
   this.radius = (maxRadius - minRadius) / (total + 2) * (n + 1) + minRadius;
-  this.peg = 0;  // 0, 1, 2
+  this.peg = 0;  // 0, 1, or 2
   this.isRaised = false;
   this.x = undefined;
   this.y = undefined;
@@ -235,8 +239,7 @@ function Disk(n, total) {
   this.sourceY = undefined;
   this.hasMoved = false;
   this.isFirstMove = true;
-  pegs[this.peg].push(this);
-  disks.push(this);
+
   if (n === 0) {
     // Orange top block.
     this.fillColour = '#F0A609';
@@ -250,9 +253,13 @@ function Disk(n, total) {
     this.fillColour = '#2E5FFF';
     this.strokeColour = '#1B3899';
   }
+
+  pegs[this.peg].push(this);
   this.setTarget();
 }
 
+// Draw this disk in the context's current x/y location.
+// Provide the pitch and roll in degrees.
 Disk.prototype.drawDisk = function(pitchDegrees, rollDegrees) {
   if (pitchDegrees < 0) pitchDegrees += 360;
   if (rollDegrees < 0) rollDegrees += 360;
@@ -263,34 +270,37 @@ Disk.prototype.drawDisk = function(pitchDegrees, rollDegrees) {
   ctx.strokeStyle = this.strokeColour;
   ctx.lineWidth = 1;
 
+  var radius = this.radius;
   var ratio = Math.sin(pitchRadians);
-  var height = Math.abs(this.radius * ratio);
+  var height = Math.abs(radius * ratio);
   var halfVisualThickness = Math.cos(pitchRadians) * ((DISK_THICKNESS - 1) / 2);
   var topY = pitchDegrees > 180 ? halfVisualThickness : -halfVisualThickness;
   var bottomY = -topY;
 
   ctx.rotate(rollRadians);
   ctx.beginPath();
-  ctx.ellipse(0, bottomY, this.radius, height, 0, 0, 2 * Math.PI);
+  ctx.ellipse(0, bottomY, radius, height, 0, 0, 2 * Math.PI);
   ctx.fill();
   ctx.stroke();
 
   ctx.beginPath();
-  ctx.fillRect(-this.radius, topY, this.radius * 2, bottomY - topY);
+  ctx.fillRect(-radius, topY, radius * 2, bottomY - topY);
   ctx.fill();
   ctx.beginPath();
-  ctx.moveTo(-this.radius, topY);
-  ctx.lineTo(-this.radius, bottomY);
-  ctx.moveTo(this.radius, topY);
-  ctx.lineTo(this.radius, bottomY);
+  ctx.moveTo(-radius, topY);
+  ctx.lineTo(-radius, bottomY);
+  ctx.moveTo(radius, topY);
+  ctx.lineTo(radius, bottomY);
   ctx.stroke();
 
   ctx.beginPath();
-  ctx.ellipse(0, topY, this.radius, height, 0, 0, 2 * Math.PI);
+  ctx.ellipse(0, topY, radius, height, 0, 0, 2 * Math.PI);
   ctx.fill();
   ctx.stroke();
 };
 
+// The disk has been raised, lowered, or moved to a new peg.
+// This function will set the new target location for the following move.
 Disk.prototype.setTarget = function() {
   this.targetX = pegX[this.peg];
   this.targetY = pegs[this.peg].indexOf(this) * DISK_THICKNESS;
@@ -307,17 +317,24 @@ Disk.prototype.setTarget = function() {
   }
 };
 
+// Draw a landing spot for one peg.
 function drawSpot() {
-  ctx.strokeStyle = '#ccc';
-  ctx.lineWidth = 1;
+  // Find the radius of the largest disk.
+  var radius = 0;
+  for (var i = 0; i < pegs.length; i++) {
+    if (pegs[i][0]) {
+      radius = Math.max(radius, pegs[i][0].radius);
+    }
+  }
 
   var pitchRadians = VIEW_ANGLE / 180 * Math.PI;
   var ratio = Math.sin(pitchRadians);
-  var radius = disks[0].radius;
   var height = Math.abs(radius * ratio);
   var halfVisualThickness = Math.cos(pitchRadians) * ((DISK_THICKNESS - 1) / 2);
   var bottomY = VIEW_ANGLE > 180 ? -halfVisualThickness : halfVisualThickness;
 
+  ctx.strokeStyle = '#ccc';
+  ctx.lineWidth = 1;
   ctx.beginPath();
   ctx.ellipse(0, bottomY, radius, height, 0, 0, 2 * Math.PI);
   ctx.stroke();
